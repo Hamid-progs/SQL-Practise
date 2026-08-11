d7c477c6f9f4d472afbb65dd94af1d9603552e8f
MUHAMMAD HAMID RAZA <m.hamidrazashahid321@gmail.com>
2026-08-11 20:12:19 +0500

    leet code 1068 solution file

---
+++ 1068-Product_Sales_Analysis_I.sql
@@ -0,0 +1,6 @@
+-- link: https://leetcode.com/problems/product-sales-analysis-i
+
+-- Write your MySQL query statement below
+select p.product_name,s.year,s.price from sales as s
+Join Product as p 
+on s.product_id = p.product_id;
