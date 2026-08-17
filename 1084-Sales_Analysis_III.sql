75ad1ad73097cc17376debd0a4957ad9fc22f7c9
MUHAMMAD HAMID RAZA <m.hamidrazashahid321@gmail.com>
2026-08-17 21:55:20 +0500

    leet code 1084 solution file

---
+++ 1084-Sales_Analysis_III.sql
@@ -0,0 +1,10 @@
+-- link: https://leetcode.com/problems/sales-analysis-iii
+
+-- Write your MySQL query statement below
+select product_id,product_name from Product where product_id in
+(
+    select product_id from Sales group by product_id
+    having
+    Min(sale_date) >=  '2019-01-01' and 
+    Max(sale_date) <=  '2019-03-31'
+);
